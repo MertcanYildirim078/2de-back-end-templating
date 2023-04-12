8fc642e15772183e42a83199e5ebc0b4060947f2
MertcanYildirim078 <99068314@mydavinci.nl>
2023-04-12 12:30:51 +0200

    Onderdeel 3 af templating

---
+++ onderdeel3/content.php
@@ -0,0 +1,13 @@
+<?php 
+
+$contentImgHtml = ' <img src="./apex.png">
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Id minus debitis adipisci ab similique ut rem illo ex.
+      Minima, porro dolores! Facere nihil autem illo, ut consequatur unde tempore expedita,
+     laudantium illum provident ipsa ex vitae accusantium repudiandae dolore praesentium ab in architecto soluta incidunt recusandae?
+      Possimus dicta explicabo porro.
+</p>
+';
+
+?>
